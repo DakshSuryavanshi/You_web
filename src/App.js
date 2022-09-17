cc2c963009e925f888b6cc62238c08510b6c906b
yashs9131 <yashs9131@gmail.com>
2022-09-17 16:56:58 +0530

    Installed dependencies and base code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>You.AI Web app</h1>
+            </div>
+        )
+    }
+
+}
+
+
+export default App;
